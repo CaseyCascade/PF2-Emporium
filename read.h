e10fe955c47984e2a5ca4493726659970a28a28b
CaseyCascade <39325725+CaseyCascade@users.noreply.github.com>
2024-04-03 09:20:10 -0700

    Fixed OtherSources Bug

--- read.h
+++ read.h
@@ -25,60 +25,60 @@ string temp;
 void initialize (json item, Item &newItem){
     for (auto &i : item.items()) //Individual Item 
     {
-        if (!ignore_var(i.key()))
-        {
-            // Recursion // 
-            if (i.value().is_object()) initialize (i.value(), newItem); // Recursive Call into SubObject
-
-             // Hard Coded Data Entries //
-            else if (i.key() == "name") newItem.setName(i.value());
-            else if (i.key() == "source") newItem.addSource(i.value()); 
-            else if (i.key() == "page") newItem.setPage(i.value()); 
-            else if (i.key() == "level") newItem.setLevel(i.value());
-            else if (i.key() == "amount") newItem.setPrice(i.value()); 
-            else if (i.key() == "coin") newItem.setCoin(i.value()); 
-            else if (i.value().is_boolean()) newItem.addTrait(i.key()); 
+        if (ignore_var(i.key())) continue; 
+        
+        // Recursion // 
+        if (i.value().is_object()) initialize (i.value(), newItem); // Recursive Call into SubObject
+        
+        // Hard Coded Data Entries //
+        else if (i.key() == "name") newItem.setName(i.value());
+        else if (i.key() == "source") newItem.addSource(i.value()); 
+        else if (i.key() == "otherSources") newItem.addSource(i.value());
+        else if (i.key() == "page") newItem.setPage(i.value()); 
+        else if (i.key() == "level") newItem.setLevel(i.value());
+        else if (i.key() == "amount") newItem.setPrice(i.value()); 
+        else if (i.key() == "coin") newItem.setCoin(i.value()); 
+        else if (i.value().is_boolean()) newItem.addTrait(i.key()); 
             
-            // Cases of Possible Differing Data Types //
-            else if (i.key() == "entries")  //There are sometimes sub-objects here, but we're choosing to ignore them
+        // Cases of Possible Differing Data Types //
+        else if (i.key() == "entries")  //There are sometimes sub-objects here, but we're choosing to ignore them
+        {
+            for (auto &j : i.value()) if (j.is_string()) newItem.addEntry(j); 
+        }
+        else if (i.key() == "bulk")
+        {
+            if (i.value().is_string())
             {
-                for (auto &j : i.value()) if (j.is_string()) newItem.addEntry(j); 
+                if (i.value() == "L") newItem.setBulk(0.1); 
+                else newItem.setBulk(0); 
             }
-            else if (i.key() == "bulk")
-            {
-                if (i.value().is_string())
-                {
-                    if (i.value() == "L") newItem.setBulk(0.1); 
-                    else newItem.setBulk(0); 
-                }
-                else newItem.setBulk(i.value()); 
-            }
-            else if (i.key() == "hands")
-            {
-                if (i.value().is_string()) temp = i.value();
-                else temp = to_string(i.value());
-                temp += " hand(s)"; 
-                newItem.addTrait(temp); 
-                temp.clear(); 
-            }
+            else newItem.setBulk(i.value()); 
+        }
+        else if (i.key() == "hands")
+        {
+        if (i.value().is_string()) temp = i.value();
+            else temp = to_string(i.value());
+            temp += " hand(s)"; 
+            newItem.addTrait(temp); 
+            temp.clear(); 
+        }
 
-            // Trait Entries //
-            else if (i.value().is_string()) newItem.addTrait(i.value()); 
-            else if (i.value().is_array() && i.key() != "entries") //Array
+        // Trait Entries //
+        else if (i.value().is_string()) newItem.addTrait(i.value()); 
+        else if (i.value().is_array() && i.key() != "entries") //Array
+        {
+            for (auto &j : i.value())
             {
-                for (auto &j : i.value())
+                if (j.is_string()) 
                 {
-                    if (j.is_string()) 
-                    {
-                        if (i.key() == "originally" || i.key() == "reprinted") newItem.addSource(j);
-                        else newItem.addTrait(j);
-                    }
-                    else 
-                    {
-                        cout << "ERROR: Found a non-string in an array\n"; 
-                        cout << "KEY: " << i.key() << " VALUE: " << j << endl; 
-                        exit(0); 
-                    }
+                    if (i.key() == "Originally" || i.key() == "Reprinted") newItem.addSource(j);
+                    else newItem.addTrait(j);
+                }
+                else 
+                {
+                    cout << "ERROR: Found a non-string in an array\n"; 
+                    cout << "KEY: " << i.key() << " VALUE: " << j << endl; 
+                    exit(0); 
                 }
             }
         }
@@ -90,6 +90,7 @@ void read(){
     json input = json::parse(baseItems);
     vector <Item> items; 
     Item newItem;  
+    int count = 0;
 
     for (auto &i : input) // Each Array is an Input File's set of Items 
     {
