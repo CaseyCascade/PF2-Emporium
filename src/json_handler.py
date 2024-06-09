19d2a8922141f1c34b65817be5f241e7a09cf952
Mattheau Casey <mattheauryan@gmail.com>
2024-06-08 21:29:58 -0700

    Beginning Rewrite. Started json_handler.py

---
+++ src/json_handler.py
@@ -0,0 +1,37 @@
+import json 
+import os 
+
+
+class Item: 
+    def __init__(self, name):
+        return 
+    def write_to_file(filepath):
+        return 
+
+def write_items_to_file(json_file_path): #TODO
+    print("Yup")
+    return
+
+def file_exists_in_data(filename, data_dir):
+    for file in os.listdir(data_dir):
+        name, extension = os.path.splitext(file)
+        if filename == name and extension == '.txt':
+            return True
+    return False 
+
+def check_for_new_json_files(json_dir, data_dir):
+    for file in os.listdir(json_dir):
+        name, extension = os.path.splitext(file)
+        if extension != '.json': 
+            continue 
+        if not file_exists_in_data(name, data_dir):
+            write_items_to_file(os.path.abspath(file))
+    return 
+
+json_directory = 'json_input'
+data_directory = 'data/core_items'
+
+full_json_path = os.path.abspath(json_directory)
+full_data_path = os.path.abspath(data_directory)
+
+check_for_new_json_files(full_json_path, full_data_path)
---
+++ src/traitid.cpp
@@ -0,0 +1,3 @@
+#include "traitid.h"
+
+TraitID :: TraitID() {}
