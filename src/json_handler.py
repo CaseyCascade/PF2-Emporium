e1b3593d9db46cb823a796a5e7e56d1f35de1ff2
CaseyCascade <39325725+CaseyCascade@users.noreply.github.com>
2024-06-10 17:14:11 -0700

    Begun work on json parsing

--- src/json_handler.py
+++ src/json_handler.py
@@ -1,15 +1,78 @@
 import json 
 import os 
 
+trait_keys = ['category', 'subCategory', 'type', 'damageType', 'group', ]
 class Item: 
-    def __init__(self, name):
+    name = None
+    page = None
+    gold = None
+    level = None
+    bulk = None
+    sources = []
+    entries = []
+    traits = []
+    remaster = False
+
+    def __init__(self):
         return 
+    
+    def set_name(self, name):
+        self.name = name
+
+    def set_page(self, page):
+        self.page = page
+        
+    def set_gold (self, coin, amount): # TODO change to allow the key value to be passed through instead
+        if coin == 'pp':
+            self.gold = amount * 10
+        elif coin == 'gp':
+            self.gold = amount
+        elif coin == 'sp':
+            self.gold = amount/10
+        elif coin == 'cp': 
+            self.gold = amount/100
+        else:
+            print("Coin Value Not Recognized: ", coin)
+
+    def set_level (self, level):
+        self.level = level 
+
+    def set_bulk (self, bulk): #TODO needs specific entry case similar to set_gold
+        self.bulk = bulk 
+
+    def add_source(self, source):
+        self.sources.append(source)
+
+    def add_entry(self, entry):
+        self.entries.append(entry)
+
+    def add_trait(self, trait):
+        self.traits.append(trait)
+
+    def set_remaster(self):
+        self.remaster = True
+
     def write_to_file(filepath):
         return 
+    
+def walk(data):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            print(f'Key: {key}')
+            # print(f'Key: {key}, Value: {value}')
+            walk(value)
+    elif isinstance(data, list):
+        for index, item in enumerate(data):
+            #print(f'Index: {index}, Item: {item}')
+            walk(item)
+    #else:
+        #print(f'Value: {data}')
 
 def write_items_to_file(json_file_path): #TODO
-    print("Yup")
-    return
+    print(json_file_path)
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+        walk(data)
 
 def file_exists_in_data(filename, data_dir):
     for file in os.listdir(data_dir):
@@ -24,14 +87,14 @@ def check_for_new_json_files(json_dir, data_dir):
         if extension != '.json': 
             continue 
         if not file_exists_in_data(name, data_dir):
-            write_items_to_file(os.path.abspath(file))
+            write_items_to_file(os.path.join(json_dir, file))
     return 
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
+repo_directory = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__)))) 
 json_directory = 'json_input'
 data_directory = 'data/core_items'
 
-full_json_path = os.path.join(script_directory, json_directory)
-full_data_path = os.path.join(script_directory, data_directory)
+full_json_path = os.path.join(repo_directory, json_directory)
+full_data_path = os.path.join(repo_directory, data_directory)
 
 check_for_new_json_files(full_json_path, full_data_path)
