dafea44d4edc992850a34c307c749485080b1ca0
Mattheau Casey <mattheauryan@gmail.com>
2024-06-13 16:49:22 -0700

    Finished Writing to .txt. Needs trait parsing

--- src/json_handler.py
+++ src/json_handler.py
@@ -1,6 +1,8 @@
 import json 
 import os 
 
+debug = True
+
 class Item: 
     name = None
     page = None
@@ -8,12 +10,12 @@ class Item:
     level = None
     bulk = None
     source = None
-
     price = None
     coin = None
-    entries = []
+    entry = None
     traits = []
     remaster = False
+    lock_entry = False
 
     def __init__(self):
         return 
@@ -61,10 +63,11 @@ class Item:
     def set_source(self, source):
         self.source = source
 
-    def add_entry(self, entry):
-        self.entries.append(entry)
+    def set_entry(self, entry):
+        self.entry = entry 
+        self.lock_entry = True
 
-    def add_trait(self, trait):
+    def add_trait(self, trait): #TODO we need to do some string parsing on input traits (Crossbow|PC1 && two-hand <d8>)
         self.traits.append(trait)
 
     def set_remaster(self):
@@ -80,10 +83,45 @@ class Item:
         self.entries = []
         self.traits = []
         self.remaster = False
+        self.lock_entry = False
+
+    def print(self):
+        print("Name: ", self.name)
+        print("Source: ", self.source)
+        print("Page: ", self.page)
+        print("Gold: ", self.gold)
+        print("Level: ", self.level)
+        print("Bulk: ", self.bulk)
+        print("Entry: ", self.entry)
+        print("Traits: ")
+        for trait in self.traits:
+            print(trait)
+        print()
+        
 
     def write_to_file(self, filepath):
         if not self.name:
             return 
+        with open(filepath, 'a') as file:
+            file.write("NAME\n")
+            file.write(self.name)
+            file.write("\nSOURCE\n")
+            file.write(self.source)
+            file.write("\nPAGE\n")
+            file.write(str(self.page))
+            file.write("\nGOLD\n")
+            file.write(str(self.gold))
+            file.write("\nLEVEL\n")
+            file.write(str(self.level))
+            file.write("\nBULK\n")
+            file.write(str(self.bulk))
+            file.write("\nENTRY\n")
+            file.write(self.entry)
+            file.write("\nTRAITS\n")
+            for trait in self.traits:
+                file.write(trait)
+                file.write('\n')
+            file.write('\n')
         return 
     
 def process_data(item: Item, key, value): #TODO Needs testing
@@ -101,13 +139,10 @@ def process_data(item: Item, key, value): #TODO Needs testing
         item.set_bulk(value)
     elif key == 'source':
         item.set_source(value)
-    elif key == 'entries':
-        item.add_entry(value)
-    elif key in trait_keys:
+    elif key == 'entries' and not item.lock_entry:
+        item.set_entry(value)
+    elif key in trait_keys: 
         item.add_trait(value)
-        
-
-        
     
 def walk_item(data, new_item, previous_key):
     if isinstance(data, dict):
@@ -128,7 +163,10 @@ def process_file(json_file_path):
             if isinstance(value, list):
                 for index, item in enumerate(value):
                     walk_item(item, instance, None)
-                    instance.write_to_file(full_data_path)
+                    file_name = os.path.splitext(os.path.basename(json_file_path))[0]
+                    file_name = file_name + '.txt'
+                    data_file_path = os.path.join(full_data_path, file_name)
+                    instance.write_to_file(data_file_path)
                     instance.clear()
     else:
         print("ERROR: Unusual JSON Formatting - No Outer List")
@@ -146,10 +184,15 @@ def check_for_new_json_files(json_dir, data_dir):
         if extension != '.json': 
             continue 
         if not file_exists_in_data(name, data_dir):
+            if debug:
+                print("Processing ", file)
             process_file(os.path.join(json_dir, file))
+        else:
+            if debug: 
+                print(file, "already exists as a .txt. Will not be updated.")
     return 
 
-trait_keys = ['category', 'subCategory', 'type', 'damageType', 'group', ]
+trait_keys = ['category', 'subCategory', 'damage', 'damageType', 'group', 'traits', 'access']
 new_item = Item()
 
 repo_directory = os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__)))) 
